64b1d57d2696340eea246a19bcfb20f98fdfac2e
David Calle <calle.d@javeriana.edu.co>
2016-06-01 00:28:30 +0000

    Angular

--- compu2/www/js/app.js
+++ compu2/www/js/app.js
@@ -1,9 +1,14 @@
 var app = angular.module( 'universidad' , ['ionic', 'ui.router', 'ngCordova' ]);
+
+var m_degree = 111320;
+var RANGE = 0.00005 ;// five meters
 var preguntas = [
 {
   id: 1 ,
   valor: 10,
-  pregunta : "cómo estás?",
+  pregunta : "como estas?",
+  checked:"false",
+  explicacion: "la respuesta correcta es bien",
   respuestas : [
   {
     respuesta : "bien",
@@ -14,88 +19,246 @@ var preguntas = [
     correcta : false
   }
   ]
-  
-}
-,
+},
 { 
   id: 2 ,
   valor: 10,
-  pregunta : "¿Que es Mineria de Datos?",
+  pregunta : "Que es Mineria de Datos?",
+  checked:"false",
+  explicacion: "la respuesta correcta es Materia muy importante",
   respuestas : [
   {
     respuesta : "Un rellenazo",
-    correcta : true
+    correcta : false
   },
   {
     respuesta : "Materia muy importante",
+    correcta : true
+  }
+  ]
+},
+{ 
+  id: 3 ,
+  valor: 40,
+  pregunta : "Complejidad de la busqueda binaria?",
+  checked:"false",
+  explicacion: "la respuesta correcta es Log(n)",
+  respuestas : [
+  {
+    respuesta : "n a la 2",
     correcta : false
+  },
+  {
+    respuesta : "Log(n)",
+    correcta : true
   }
   ]
 }
 ];
-
 var examenes = [
   {
     id: 1,
-    nombre: "Parcial 1 Cálculo",
-    descripcion: "1 hora, También puede ser tomado presencial en el salón 2-305. Importante llevar cuaderno, lapiz y borrador",
+    nombre: "Parcial 1 Calculo",
+    descripcion: "1 hora, Tambien puede ser tomado presencial en el salon 2-305. Importante llevar cuaderno, lapiz y borrador",
     tema : "Derivadas",
+    eliminar : false,
     preguntas : [
       {
         id: 1 ,
         valor: 50
       },{
         id: 2,
-        valor: 70
+        valor: 50
       }
     ]
   },
   {
     id: 2,
-    nombre: "Parcial 3 Computación Móvil",
-    descripcion: "3 horas, El parcial consiste en hacer una aplicación móvil sencilla. Solo se puede usar phonegap",
+    nombre: "Parcial 3 Computacion Movil",
+    descripcion: "3 horas, El parcial consiste en hacer una aplicacion movil sencilla. Solo se puede usar phonegap",
     tema : "iOS, PhoneGap y Patrones",
+    eliminar : false,
     preguntas : [
       {
         id: 1 ,
         valor: 50
       },{
         id: 2,
-        valor: 70
+        valor: 50
       }
     ]
-  },{
+  },
+  {
     id: 3,
-    nombre: "Bioquímica",
-    descripcion: "3 horas, el parcial es un domingo a las 7am antes de un festivo. 30 preguntas, 20 minutos para resolverlo. Pasa aproximadamante el 1% de los estudiantes",
+    nombre: "Bioquimica",
+    descripcion: "3 horas, el parcial es un domingo a las 7am antes de un festivo. 30 preguntas, 20 minutos para resolverlo.",
     tema : "TODO lo visto desde primer semestre. ",
+    eliminar : false,
     preguntas : [
       {
         id: 1 ,
         valor: 50
       },{
         id: 2,
-        valor: 70
+        valor: 50
+      }]
+  },
+  {
+    id: 4,
+    nombre: "Analisis de Algoritmos",
+    descripcion: "Pasas o pasas.",
+    tema : "Programacion Dinamica",
+    eliminar : false,
+    preguntas : [
+      {
+        id: 1 ,
+        valor: 25
+      },{
+        id: 3,
+        valor: 75
       }]
   }
 ];
+
+var examXubicacion = 
+[
+  {
+    idEval: 2,
+    nombre: "Facultad Ingenieria",
+    lat: 4.626978,
+    lon: -74.064081,
+    radio: 27 / m_degree,
+  },
+  {
+    idEval: 4,
+    nombre: "Facultad Ingenieria",
+    lat: 4.626713,
+    lon: -74.06409,
+    radio: 27 / m_degree,
+  },
+  {
+    idEval: 3,
+    nombre: "Tepanyaki",
+    lat: 4.626978,
+    lon: -74.064081,
+    radio: 8 / m_degree,
+  },
+  {
+    idEval: 1,
+    nombre: "Facultad de artes",
+    lat: 4.6267,
+    lon: -74.06413,
+    radio: 35 / m_degree,
+  },
+  {
+    idEval: 4,
+    nombre: "Edificio Fernando Baron",
+    lat: 4.6266522,
+    lon: -74.06407,
+    radio: 16 / m_degree,
+  }
+];
+
 var respuestas = [];
 
 /**
  * Controlador de la pagina principal
  * 
  * */
-app.controller('MainController', function($scope) { // el scope es como una variable global en angular, se puede crear un controlador por vista o conjunto de vistas, como te quede más fácil
+
+ var preguntasAElegir=[];
+ var preElegidas=[];
+ var reguntas=[];
+ var examenTemporal={reguntas:[] };
+ var total=0;
+
+app.controller('ExamenesController', function($scope) { 
+ 
+  
+  preguntasAElegir=darPreguntas();
+  $scope.preguntas=[];
+  $scope.preguntas=preguntasAElegir.slice(0);
+ 
+  $scope.elegidas=[];
+  $scope.elegidas=preElegidas.slice(0);
+  
+  $scope.examenes=[];
+  $scope.examenes=obtenerExamenes();
+
+ $scope.examenAGuardar=examenTemporal.nombre;
+  $scope.totalexamen=0;
+  $scope.totalexamen=total;
+  $scope.eliminarExamen=function(){
+    
+    for(var j=0; j<$scope.examenes.length;j++)
+    {
+      if($scope.examenes[j].eliminar==true){
+      
+        $scope.examenes.splice(j,1);
+        
+        j--;
+      }
+    }
+    almacenarExamenes($scope.examenes);
+  }
+  
+  $scope.mostrarNombre=function(){
+    examenTemporal.nombre=$scope.nombresito;
+   
+    examenTemporal.descripcion=$scope.descripcion;
+    examenTemporal.tema=$scope.tema;
+  }
+   
+
+  $scope.agregarPre=function()
+  {
+    
+      for( var j  = 0 ; j < $scope.preguntas.length ; j++ ){
+      if($scope.preguntas[j].checked==true){
+        
+          preElegidas.push($scope.preguntas[j]);
+          
+         
+      }
+    }
+  $scope.elegidas=preElegidas.slice(0);
+  }
+  
+   $scope.agregarValor=function(){
+   
+     examenTemporal.preguntas=$scope.elegidas.slice(0);
+      for(var j=0; j< examenTemporal.preguntas.length;j++){
+        total=total+Number(examenTemporal.preguntas[j].valor);
+      }
+     agregarExamen(examenTemporal);
+   
+  }
+  
+  
+  $scope.mostrarCheck=function(){
+    alert($scope.checkbox);
+  }
+   
+   
+ });
+  
+app.controller('PreguntaController', function($scope) { 
+ 
+
+  $scope.mostrarCheck=function(){
+    alert($scope.checkbox);
+  }
+   
+   
+ });
+var opciones=[];
+var pregun = { opciones:[] };
+app.controller('MainController', function($scope) { // el scope es como una variable global en angular, se puede crear un controlador por vista o conjunto de vistas, como te quede mas facil
   
   $scope.hello = "Hola mundo, prueba de angular";
-  $scope.opciones = [{
-    nombre: "Profesores",
-    url: "Una URL"
-  }, {
-    nombre: "Estudiante",
-    url: "qwerty"
-  }];
 
+  $scope.opcionesPregunta=[];
+  
   $scope.agregarProfesor = function(profesor) {
     $scope.profesores.push(profesor);
   }
@@ -109,13 +272,31 @@ app.controller('MainController', function($scope) { // el scope es como una vari
   
   console.log("la correcta es: " + $scope.correcta );
   
-  $scope.agregarPregunta = function(opcion) {
+  $scope.agregarRespuesta = function(opcion) {
     pre.value=" ";
-    $scope.opcionesPregunta.push( { opcion : opcion } ); // tienes que meter un objeto que este formado igual que el otro, osea los de la lista
+   
+    opciones.push({opcion:opcion} ); // tienes que meter un objeto que este formado igual que el otro, osea los de la lista
+    $scope.opcionesPregunta=opciones.slice(0);
+   
   }
    $scope.elegirPregunta = function(opcion) {
     
     $scope.opcionesPregunta.push( { opcion : opcion } ); // tienes que meter un objeto que este formado igual que el otro, osea los de la lista
+  }
+  
+  
+  $scope.agregarPre=function()
+  {
+         
+    pregun.pregunta=$scope.nombre;
+    pregun.valor=$scope.puntaje;
+    pregun.tema=$scope.temaPre;
+    pregun.explicacion=$scope.explicacion;
+    pregun.opciones=opciones.slice(0);
+    alert(pregun.pregunta);
+    agregarPregunta(pregun);
+    
+    
   }
   
   $scope.temas=[
@@ -129,28 +310,79 @@ app.controller('MainController', function($scope) { // el scope es como una vari
     tema: "Estructuras"
   }
 
-  ]
+  ];
+  
+  
+  
+  
 });
 
-
+app.controller( 'MyCtrl', function($scope) {
+});
 /**
  * 
  * Controlador de prueba del GPS
  *
  * */
-app.controller( 'MyCtrl' , function( $scope , $cordovaGeolocation ){
-  var watchOptions = { timeout : 3000, enableHighAccuracy: false  };
-  var watch = $cordovaGeolocation.watchPosition(watchOptions);
+app.controller( 'GpsCtrl' , function( $scope , $cordovaGeolocation )
+{
+  $scope.exmUbi = null;
   
-  watch.then( null,
-  function(err) {
-    console.log("Error");
-  },
-  function(position) {
-    var lat = position.coords.latitude;
-    var long = position.coords.longitude;
-  });
+  function buscarEvals () 
+  {
+    alert("ENTRA ALCONTROL GPS");
+    var watchOptions = { timeout : 3000, enableHighAccuracy: false  };
+    var watch = $cordovaGeolocation.watchPosition(watchOptions);
+    watch.then( null,
+      function(err) 
+      {
+        alert("Error");
+      },
+      function(position) 
+      {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        alert( "LAT: "+ lat + "LON" + long );
+        buscarExamenes( lat, lon );
+      }
+    );
   watch.clearWatch();
+  }
+  
+  function mostrarExamen( id ){
+    examenes = obtenerExamenes();
+    for( var i = 0; i < examenes.length ; i++ ){
+      examen = examenes[i];
+      if( examen.id == id ){
+        return examen;
+      }
+    }
+    return null;
+  }
+  
+  function buscarExamenes ( lat, lon )
+  {
+     alert("LLEGA  ABUSCAR");
+     var latitudLeft = lat - RANGE;
+     var latitudRight = lat + RANGE;
+     var longitudeUp = lon + RANGE;
+     var longitudeDown = lon - RANGE;
+     
+     for( var i = 0; i < examXubicacion.length; i++ )
+     {
+       var parc = examXubicacion[i];
+       if( parc.lon + parc.radio >longitudeDown &&
+           parc.lon -parc.radio < longitudeUp &&
+           parc.lat +  parc.radio > latitudLeft &&
+          parc.lat -  parc.radio < latitudRight )
+       {
+        var ex = mostrarExamen( parc.idEval );
+        alert( "Estas en " + parc.nombre + "presenta la evaluacion: " + ex.nombre );
+        $scope.exmUbi.push( ex );
+       }
+     }
+  }
+    $scope.buscarEvals = buscarEvals();
 });
 
 
@@ -159,25 +391,23 @@ app.controller( 'MyCtrl' , function( $scope , $cordovaGeolocation ){
  * Controlador de Tomar un Examen 
  * 
  */
-app.controller('TomarExamen', function( $state ,$stateParams ,  $scope , $cordovaCamera ){
+app.controller('TomarExamen', function( $state , $stateParams , $scope , $cordovaCamera ){
   
   //////////////// FUNCIONALIDAD DE FOTOS //////////////////////////////
   //alert( $stateParams.id )
   $scope.preguntas = [];
-  $scope.evaluacion = { preguntas:[] };
+  //$scope.evaluacion = { preguntas:[] };
   
   var id = $stateParams.id;
   if( id != null ){
     var examen = mostrarExamen( id );
-    $scope.examen = examen;
-    almacenarPreguntas( preguntas );
+    $scope.examenApresentarReal = null;
+    $scope.examenApresentarReal = examen;
+
     var preg = obtenerPreguntas( examen );
     for( var j  = 0 ; j < preg.length ; j++ ){
-      $scope.evaluacion.preguntas.push( preg[ j ]);
+      $scope.examenApresentarReal.preguntas.push( preg[ j ]);
     }
-    $state.go($state.current, {}, {reload: true});
-    
-    
   }
   
   $scope.takePhoto = function () {
@@ -227,12 +457,12 @@ app.controller('TomarExamen', function( $state ,$stateParams ,  $scope , $cordov
   }
   
   function obtenerPreguntas( examen ) {
-    preguntas = darPreguntas();
+    var pregntas = darPreguntas();
     retorno = [];
     for( var i = 0 ;  i < examen.preguntas.length ; i++ ){
-      for( var j = 0; j < preguntas.length ; j++){
-        if( examen.preguntas[i].id == preguntas[j].id ){
-          retorno.push( preguntas[j] );
+      for( var j = 0; j < pregntas.length ; j++){
+        if( examen.preguntas[i].id == pregntas[j].id ){
+          retorno.push( pregntas[j] );
         }
       }
     }
@@ -252,7 +482,7 @@ app.controller('TomarExamen', function( $state ,$stateParams ,  $scope , $cordov
   function mostrarExamen( id ){
     examenes = obtenerExamenes();
     for( var i = 0; i < examenes.length ; i++ ){
-      examen = examenes[ i ];
+      examen = examenes[i];
       if( examen.id == id ){
         return examen;
       }
@@ -261,51 +491,99 @@ app.controller('TomarExamen', function( $state ,$stateParams ,  $scope , $cordov
   }
   $scope.mostrarExamen = mostrarExamen;
   $scope.mostrarExamenesDisponibles = mostrarExamenesDisponibles;
-  $scope.examenes = examenes;
+  $scope.examenes = examenes; 
   $scope.examen = null;
-  
-  
-  
 });
 
 /**
  * Controlador del codigo QR
  * 
  */
-app.controller('qr', function($scope, $rootScope, $cordovaBarcodeScanner, $ionicPlatform) {
+app.controller( 'qr', function($scope, $rootScope, $cordovaBarcodeScanner, $ionicPlatform, $http ) 
+{
+        function construirExamen ( datos )
+        {      
+          var examen = new Object();
+          var pregunta = new Object();
+          var respuesta = new Object();          
+          
+          examen.id = obtenerCantidadExamenes() + 1;
+          examen.nombre = datos.nombre;
+          examen.descripcion = datos.descripcion;
+          examen.tema = datos.tema;
+          examen.eliminar = datos.eliminar;
+          examen.preguntas = [];
+           
+          for( var i = 0; i < datos.tamPreguntas; i++ )
+          {
+            pregunta.id = obtenerCantidadPreguntas() + 1;
+            pregunta.pregunta = datos.preguntas[i].texto;  
+            pregunta.checked = datos.preguntas[i].checked;
+            pregunta.valor = datos.preguntas[i].valor; 
+            pregunta.respuestas = datos.preguntas[i].respuestas;
+            pregunta.explicacion = datos.preguntas[i].explicacion;
+            
+/*            for( var j = 0; j < datos.tamRespuestas; j++ )
+            {
+              pregunta.respuestas[j] = datos.preguntas[i].respuestas[j];
+            }*/
+            agregarPregunta( pregunta );
+            examen.preguntas[i] = new Object();
+            examen.preguntas[i].id = pregunta.id;
+            examen.preguntas[i].valor = pregunta.valor;
+          }
+          agregarExamen( examen );
+          alert("Se ha registrado un nuevo examen bajo el nombre: " + examen.nombre );
+          //$scope.examenes = obtenerExamenes();
+        }
         var vm = this;
-        $scope.scanResults = "No se ha escaneado ningún código";
+        $scope.scanResults = "No se ha escaneado ningun codigo";
         $scope.url = null;
         $scope.scan = function(){
           console.log("Entra")
                 $cordovaBarcodeScanner
                     .scan()
-                    .then(function(result) {
+                    .then(function(result) 
+                    {
                         // Success! Barcode data is here
-                        $scope.scanResults = "Se obtuvo la url \n" +
+                        $scope.scanResults = "Se obtuvo la \n" +
                         "url: " + result.text + "\n";
                         $scope.url = result.text;
-                    }, function(error) {
+                    }, function(error) 
+                    {
                         // An error occurred
                         $scope.scanResults = 'Error: ' + error;
+                        alert("Error al escanear el codigo QR")
                     });
-        };
+            };
         
         $scope.agregarQR= function(){
-          alert("Se agrega a \n" + $scope.url);
+          $http
+          ({
+              method: 'GET',
+              url: $scope.url
+          }).then(
+          function successCallback( response ) 
+          {
+              construirExamen( response.data  );	
+          }, 
+          function errorCallback(response) 
+          {
+              alert( "Error al obtener el examen!!" );     
+          });          
           $scope.url = null;
         }
         vm.scanResults = '';
-});
-
-
-
+        });
 
 /**
  * 
  * este modulo se encarga de enrutar las vistas
  */
-app.config(function($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider, $httpProvider ) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  
         $stateProvider
           .state('index', {
             url: '/',
@@ -327,11 +605,35 @@ app.config(function($stateProvider, $urlRouterProvider) {
           }).state('creareva', {
             url: '/profesor/crear-evaluacion',
             templateUrl: 'creareva.html',
-            controller: 'MainController'
+            controller: 'ExamenesController'
+          }).state('examenPorUbicacion', {
+            url: '/examenPorUbicacion',
+            templateUrl: 'examenPorUbicacion.html',
+            controller: 'GpsCtrl'
+          }).state('secciontemas', {
+            url: '/profesor/secciontemas',
+            templateUrl: 'secciontemas.html',
+            controller: 'ExamenesController'
+          }).state('valorpre', {
+            url: '/profesor/valorpre',
+            templateUrl: 'valorpre.html',
+            controller: 'ExamenesController'
           }).state('agregarpregunta', {
             url: '/profesor/agregar-pregunta',
             templateUrl: 'agregarpregunta.html',
             controller: 'MainController'
+          }).state('eliminarev', {
+            url: '/profesor/eliminar-evaluacion',
+            templateUrl: 'eliminarev.html',
+            controller: 'ExamenesController'
+          }).state('ev-presentadas', {
+            url: '/profesor/ev-presentadas',
+            templateUrl: 'ev-presentadas.html',
+            controller: 'ExamenesController'
+          }).state('info-evaluacion', {
+            url: '/profesor/info-evaluacion',
+            templateUrl: 'info-evaluacion.html',
+            controller: 'ExamenesController'
           }).state('camara', {
             url: '/camara',
             templateUrl: 'camara.html',
--- compu2/www/js/db.js
+++ compu2/www/js/db.js
@@ -1,46 +1,116 @@
-function almacenarExamenes( examenes ){
+function almacenarExamenes( examenes )
+{
 	localStorage.setItem('examenes', JSON.stringify( examenes ) );
 }
 
-function agregarExamen( examen ){
+function agregarExamen( examen )
+{
 	mexamenes = obtenerExamenes();
 	mexamenes.push( examen )
 	examenes = mexamenes;
 	almacenarExamenes( examenes );
 }
 
-function obtenerExamenes(){
+function obtenerCantidadExamenes()
+{
 	var cadenaExamenes = localStorage.getItem('examenes');
+	var examenes = JSON.parse( cadenaExamenes );
+	return examenes.length;
+}
+
+function obtenerExamenes()
+{
+	cadenaExamenes = localStorage.getItem('examenes');
 	return JSON.parse( cadenaExamenes );
 }
 
 function inicializarDB(){
+	
+	var copiaPreg = darPreguntas();
+	if( copia == null || copia.length == 0 )
+	{
+		almacenarPreguntas( preguntas );
+	}
+	
 	var copia = obtenerExamenes();
 	if( copia == null || copia.length == 0 ){
 		almacenarExamenes( examenes );
 	}
+	
+	var copiaUbi = darUbicaciones();
+	if( copiaUbi == null || copiaUbi.length == 0 ){
+		almacenarUbicaciones( examXubicacion );
+		alert("Almaceno ubis");
+	}
+	
+    preguntas = darPreguntas();
     examenes = obtenerExamenes();
+    examXubicacion = darUbicaciones();
+	
 }
 
 function crearPregunta( pregunta , valor ){
 	return { pregunta : pregunta , valor: valor}
 }
 
+function obtenerCantidadPreguntas()
+{
+	var cadenaPreguntas = localStorage.getItem('preguntas');
+	var pregs = JSON.parse( cadenaPreguntas );
+	return pregs.length;
+}
+
 function darPreguntas(){
-	var cadenaPregunta = localStorage.getItem('preguntas');
+	cadenaPregunta = localStorage.getItem('preguntas');
 	return JSON.parse( cadenaPregunta );
 }
 
-function agregarPregunta( pregunta ){
-	var mpreguntas = darPreguntas();
+function agregarPregunta( pregunta )
+{
+	mpreguntas = darPreguntas();
 	mpreguntas.push( pregunta );
 	almacenarPreguntas( mpreguntas );
 	preguntas = mpreguntas;
 }
 
-function almacenarPreguntas( preguntas ){
-	localStorage.setItem('preguntas', JSON.stringify( preguntas ))
+function almacenarPreguntas( preguntas )
+{
+	localStorage.setItem('preguntas', JSON.stringify( preguntas ));
 }
+
+
+function obtenerCantidadUbicaciones()
+{
+	var cadenaUbicaciones = localStorage.getItem('Ubicaciones');
+	var ubis = JSON.parse( cadenaUbicaciones );
+	return ubis.length;
+}
+
+function darUbicaciones(){
+	cadenaUbicacion = localStorage.getItem('Ubicaciones');
+	return JSON.parse( cadenaUbicacion );
+}
+
+function agregarUbicaion( ubicacion )
+{
+	mUbicaciones = darUbicaciones();
+	mUbicaciones.push( ubicacion );
+	almacenarUbicaciones( mUbicaciones );
+	examXubicacion = mUbicaciones;
+}
+
+function almacenarUbicaciones( Ubicaciones )
+{
+	alert(JSON.stringify( Ubicaciones ));
+	localStorage.setItem('Ubicaciones', JSON.stringify( Ubicaciones ));
+}
+
+
+
+
+
+
+
 inicializarDB();
 
 /*	
